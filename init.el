5f48a9abf7e56fd6da407ea92c8061678651dc88
snf <snf@users.noreply.github.com>
2024-12-16 15:20:58 +0000

    giving eat a shot, commenting vterm

--- init.el
+++ init.el
@@ -502,50 +502,66 @@
 (use-package ws-butler
   :hook (prog-mode . ws-butler-mode)
   )
-(use-package vterm
-  :if (not (string= window-system 'w32))
-  :bind
-  (:map vterm-mode-map
-        ("M-y" . vterm-yank-pop)
-	)
-  (:map vterm-copy-mode-map
-	("q" . vterm-copy-mode))
-  :config
-  (defun run-in-vterm-kill (process event)
-    "A process sentinel. Kills PROCESS's buffer if it is live."
-    (let ((b (process-buffer process)))
-      (and (buffer-live-p b)
-           (kill-buffer b))))
 
-  (defun run-in-vterm (command)
-    "Execute string COMMAND in a new vterm.
 
-Interactively, prompt for COMMAND with the current buffer's file
-name supplied. When called from Dired, supply the name of the
-file at point.
 
-Like `async-shell-command`, but run in a vterm for full terminal features.
-
-The new vterm buffer is named in the form `*foo bar.baz*`, the
-command and its arguments in earmuffs.
-
-When the command terminates, the shell remains open, but when the
-shell exits, the buffer is killed."
-    (interactive
-     (list
-      (let* ((f (cond (buffer-file-name)
-                      ((eq major-mode 'dired-mode)
-                       (dired-get-filename nil t))))
-             (filename (concat " " (shell-quote-argument (and f (file-relative-name f))))))
-	(read-shell-command "Terminal command: "
-                            (cons filename 0)
-                            (cons 'shell-command-history 1)
-                            (list filename)))))
-    (with-current-buffer (vterm (concat "*" command "*"))
-      (set-process-sentinel vterm--process #'run-in-vterm-kill)
-      (vterm-send-string command)
-      (vterm-send-return)))
+(use-package eat
+  :straight (
+             :type git
+             :host codeberg
+             :repo "akib/emacs-eat"
+             :files ("*.el" ("term" "term/*.el") "*.texi"
+                     "*.ti" ("terminfo/e" "terminfo/e/*")
+                     ("terminfo/65" "terminfo/65/*")
+                     ("integration" "integration/*")
+                     (:exclude ".dir-locals.el" "*-tests.el")))
+  
   )
+
+;; (use-package vterm
+;;   :if (not (string= window-system 'w32))
+;;   :bind
+;;   (:map vterm-mode-map
+;;         ("M-y" . vterm-yank-pop)
+;; 	)
+;;   (:map vterm-copy-mode-map
+;; 	("q" . vterm-copy-mode))
+;;   :config
+;;   (defun run-in-vterm-kill (process event)
+;;     "A process sentinel. Kills PROCESS's buffer if it is live."
+;;     (let ((b (process-buffer process)))
+;;       (and (buffer-live-p b)
+;;            (kill-buffer b))))
+
+;;   (defun run-in-vterm (command)
+;;     "Execute string COMMAND in a new vterm.
+
+;; Interactively, prompt for COMMAND with the current buffer's file
+;; name supplied. When called from Dired, supply the name of the
+;; file at point.
+
+;; Like `async-shell-command`, but run in a vterm for full terminal features.
+
+;; The new vterm buffer is named in the form `*foo bar.baz*`, the
+;; command and its arguments in earmuffs.
+
+;; When the command terminates, the shell remains open, but when the
+;; shell exits, the buffer is killed."
+;;     (interactive
+;;      (list
+;;       (let* ((f (cond (buffer-file-name)
+;;                       ((eq major-mode 'dired-mode)
+;;                        (dired-get-filename nil t))))
+;;              (filename (concat " " (shell-quote-argument (and f (file-relative-name f))))))
+;; 	(read-shell-command "Terminal command: "
+;;                             (cons filename 0)
+;;                             (cons 'shell-command-history 1)
+;;                             (list filename)))))
+;;     (with-current-buffer (vterm (concat "*" command "*"))
+;;       (set-process-sentinel vterm--process #'run-in-vterm-kill)
+;;       (vterm-send-string command)
+;;       (vterm-send-return)))
+;;   )
 
 (use-package undo-fu
   :config
