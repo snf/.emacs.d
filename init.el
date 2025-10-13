08471e8fe2ecc5f52a1d3447a05f7195291e84a4
snf <snf@users.noreply.github.com>
2025-10-13 04:18:15 +0000

    ox-clip: embedding images in html so copy fully works

--- init.el
+++ init.el
@@ -868,6 +868,20 @@ results."
 
 (use-package ox-clip
   :straight (:host github :repo "jkitchin/ox-clip" :branch "master")
+  :config
+  (defun replace-in-string (what with in)
+    (replace-regexp-in-string (regexp-quote what) with in nil 'literal))
+
+  (defun org-html--format-image (source attributes info)
+    (progn
+      (setq source (replace-in-string "%20" " " source))
+      (format "<img src=\"data:image/%s;base64,%s\"%s />"
+              (or (file-name-extension source) "")
+              (base64-encode-string
+               (with-temp-buffer
+                 (insert-file-contents-literally source)
+                 (buffer-string)))
+              (file-name-nondirectory source))))
   )
 
 (use-package powershell
